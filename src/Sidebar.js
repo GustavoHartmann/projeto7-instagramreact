11ed37925377d0fe34e981329dcf2d8074808c4c
Gustavo <gustavolerner.hartmann@gmail.com>
2022-09-17 19:30:05 -0300

    Correção bug cancelar prompt nome de usuário e implementação prompt imagem usuário

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,17 +4,23 @@ import Sugestoes from "./Sugestoes";
 function Usuarios() {
   const [nome, setNome] = React.useState("Anonimo")
   const [nomePerfil, setNomePerfil] = React.useState("Anonimo")
+  const [imagemPerfil, setImagemPerfil] = React.useState("assets/img/usuario-anonimo.png")
 
   function perguntarNome() {
     let promptNome = prompt("Qual o seu nome?")
     let promptNomePerfil = prompt("Qual o nome do seu perfil?")
-    promptNome !== "" ? setNome(promptNome) : setNome("Nome inválido")
-    promptNomePerfil !== "" ? setNomePerfil(promptNomePerfil) : setNomePerfil("Nome inválido")
+    promptNome !== "" && promptNome !== null ? setNome(promptNome) : setNome("Nome inválido")
+    promptNomePerfil !== "" && promptNomePerfil !== null ? setNomePerfil(promptNomePerfil) : setNomePerfil("Nome inválido")
+  }
+
+  function perguntarImagem() {
+    let promptImagemPerfil = prompt("Qual a URL da imagem de perfil?")
+    promptImagemPerfil !== "" && promptImagemPerfil !== null ? setImagemPerfil(promptImagemPerfil) : setImagemPerfil("assets/img/usuario-anonimo.png")
   }
 
   return (
     <div className="usuario">
-      <img src="assets/img/catanacomics.svg" alt="imagem usuário"/>
+      <img src={imagemPerfil} alt="imagem usuário" onClick={perguntarImagem}/>
       <div className="texto">
         <strong>{nomePerfil}</strong>
         <span>
